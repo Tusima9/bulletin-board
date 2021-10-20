83c2c64b73da01080bad522d2665d2070b1820f1
Tusima9 <tysandsnow@gmail.com>
2021-10-20 15:43:20 +0900

    code

---
+++ client/js/main.js
@@ -0,0 +1,14 @@
+$('#post').on('click',()=>{
+
+    alert('投稿押されました')
+});
+
+$('.card .edit').on('click',()=>{
+
+    alert('put押されました')
+});
+
+$('.card .delete').on('click',()=>{
+
+    alert('delete押されました')
+});
